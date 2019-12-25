541bf84feae273a735efae530d09a48fd4d9654c
ED <eduardilgen@gmail.com>
2019-12-26 00:24:46 +0200

    ДЗ 1: Все сделано.

--- js/main.js
+++ js/main.js
@@ -7,21 +7,18 @@ const products = [
   {id: 4, title: 'Gamepad', price: 4500},
 ];
 
-const renderProduct = (title, price) => {
+const renderProduct = (title, price, img = 'https://place-hold.it/200x200') => {
   return `<div class="product-item">
             <h3>${title}</h3>
-            <p>${price}</p>
+						<p>${price}</p>
+						<img src="${img}"></img>
             <button class="by-btn">Добавить в корзину</button>
           </div>`;
 };
 
 const renderProducts = list => {
-  // const productList = list.map(product => renderProduct(product.title, product.price));
-  const productList = list.map(function (product) {
-    return renderProduct(product.title, product.price)
-  });
-  console.log(productList);
-  document.querySelector('.products').innerHTML = productList;
+	const productList = list.map(product => renderProduct(product.title, product.price, product.img));
+  document.querySelector('.products').innerHTML = productList.join('');
 };
 
 renderProducts(products);
